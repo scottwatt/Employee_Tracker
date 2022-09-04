90b3b89696a2f8d898c5de020495cf9d89f4b1f2
Scott Wattenbarger <scottwattenbarger@gmail.com>
2022-09-04 12:37:21 -0700

    adds db, server.js, gitignore, package.json, and node modules

---
+++ db/seeds.sql
@@ -0,0 +1,10 @@
+
+
+INSERT INTO department (name)
+VALUES ("Student"), ("Asgard"), ("Engineering"), ("Leader"), ("Stealth");
+
+INSERT INTO role (title, salary, department_id)
+VALUE ("Superhero", 25000.00, 2), ("God", 600000.00, 3), ("Engineer", 600000.00, 4), ("Captain", 200000.00, 1), ("Assassin", 800000.00, 5);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUE ("Peter", "Parker", 1, 3), ("Thor", "God of Thunder", 1, 1), ("Tony", "Stark", 3, 2), ("Steve", "Rogers", 5, 2), ("Natasha", "Romanova", 5, 2);
